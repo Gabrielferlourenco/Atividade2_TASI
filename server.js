c5ac4e08dfc1562fb77450e76b1b478a86723aa3
Gabrielferlourenco <gabriel.fl1999@gmail.com>
2024-04-15 23:43:43 -0300

    final commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/", function(req,resp){
+    resp.send("Bem vindo ao site!!!!")
+})
+
+app.get("/Front_End", function(req,resp){
+    resp.sendFile(__dirname + "/html/index.html")
+})
+
+app.get("/Banco_de_dados", function(req,resp){
+    resp.sendFile(__dirname + "/bdd/index.html")
+})
+
+app.get("/POO", function(req,resp){
+    resp.sendFile(__dirname + "/poo/index.html")
+})
+
+app.listen(8081, function(){
+    console.log("Servidor utilizando express")
+})
+
